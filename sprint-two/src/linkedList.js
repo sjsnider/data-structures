287b5fe96a82e253ed7f11ba4226e82ec940e09f
Hack Reactor Students <sjsnider@gmail.com>
2014-04-17 14:24:10 -0700

    finish linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,35 @@
 var makeLinkedList = function(){
   var list = {};
-  list.head = null;
-  list.tail = null;
+  list.head = {'value':null};
+  list.tail = {'value':null};
 
   list.addToTail = function(value){
+    list[value] = list.tail.value;
+    if (list[value] === null){
+      list.head.value = value;
+    }
+    list.tail.value = value;
   };
 
   list.removeHead = function(){
+    var key= list.tail.value;
+    while (list[key] !== list.head.value){
+      key = list[key];
+    }
+    delete list[list.head.value];
+    list[key] = null;
+    list.head.value = key;
   };
 
   list.contains = function(target, node){
+    var key = list.tail.value;
+    while (key !== null){
+      if (key === target){
+        return true;
+      }
+      key = list[key];
+    }
+    return false;
   };
 
   return list;
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,6 +2,8 @@ var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
   newTree.children = undefined;
+  _.extend(newTree, treeMethods);
+  window.alert(_);
   return newTree;
 };
 
