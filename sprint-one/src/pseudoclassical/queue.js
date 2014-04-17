4d6dca8947d794622f14d3806882756cc85e109a
Hack Reactor Students <sjsnider@gmail.com>
2014-04-17 15:30:30 -0700

    Renamed private variables

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -2,27 +2,27 @@ var Queue = function() {
   // Hey! Copy your code from src/prototypal/queue.js and paste it here
 
   // Use an object with numeric keys to store values
-  this.storage = {};
-  this.stackSize = 0;
+  this._storage = {};
+  this._size = 0;
 };
 
 Queue.prototype.enqueue = function(value){
-    this.storage[this.stackSize] = value;
-    this.stackSize++;
+    this._storage[this._size] = value;
+    this._size++;
   };
 
 Queue.prototype.dequeue = function(){
 
-  var result = this.storage[0];
-  delete this.storage[0];
-  for(var i=1;i<this.stackSize;i++){
-    this.storage[i-1]=this.storage[i];
+  var result = this._storage[0];
+  delete this._storage[0];
+  for(var i=1;i<this._size;i++){
+    this._storage[i-1]=this._storage[i];
   }
-  this.stackSize && this.stackSize--;
-  delete this.storage[this.stackSize];
+  this._size && this._size--;
+  delete this._storage[this._size];
   return result;
 };
 
 Queue.prototype.size = function(){
-  return this.stackSize;
+  return this._size;
 };
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -2,25 +2,25 @@ var Stack = function() {
   // Hey! Copy your code from src/functional-shared/stack.js and paste it here
 
   // Use an object with numeric keys to store values
-  this.storage = {};
+  this._storage = {};
   //debugger;
-  this.stackSize = 0; // Hint: set an initial value here
+  this._size = 0; // Hint: set an initial value here
 };
 
 
 // Implement the methods below
 Stack.prototype.push = function(value){
-  this.storage[this.stackSize] = value;
-  this.stackSize++;
+  this._storage[this._size] = value;
+  this._size++;
 };
 
 Stack.prototype.pop = function(){
-  this.stackSize &&  this.stackSize--;
-  var popped = this.storage[this.stackSize];
-  delete this.storage[this.stackSize];
+  this._size &&  this._size--;
+  var popped = this._storage[this._size];
+  delete this._storage[this._size];
   return popped;
 };
 
 Stack.prototype.size = function(){
-  return this.stackSize;
+  return this._size;
 };
