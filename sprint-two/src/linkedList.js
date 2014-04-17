df629ec35f1249a6d2d82bae572161e04a04693a
Hack Reactor Students <sjsnider@gmail.com>
2014-04-17 16:41:59 -0700

    fixing linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,35 +1,55 @@
 var makeLinkedList = function(){
   var list = {};
-  list.head = {'value':null};
-  list.tail = {'value':null};
+  list.head = null;
+  list.tail = null;
 
   list.addToTail = function(value){
-    list[value] = list.tail.value;
-    if (list[value] === null){
-      list.head.value = value;
-    }
-    list.tail.value = value;
+   var node = makeNode(value);
+
+   if(list.head === null){
+    list.head = node;
+   }else{
+    list.tail.next = node;
+   }
+
+   list.tail = node;
+
+
+
+
+
+
+    // list[value] = list.tail.value;
+    // if (list[value] === null){
+    //   list.head.value = value;
+    // }
+    // list.tail.value = value;
   };
 
   list.removeHead = function(){
-    var key= list.tail.value;
-    while (list[key] !== list.head.value){
-      key = list[key];
-    }
-    delete list[list.head.value];
-    list[key] = null;
-    list.head.value = key;
+
+    list.head = list.head.next;
+
+
+
+    // var key= list.tail.value;
+    // while (list[key] !== list.head.value){
+    //   key = list[key];
+    // }
+    // delete list[list.head.value];
+    // list[key] = null;
+    // list.head.value = key;
   };
 
   list.contains = function(target, node){
-    var key = list.tail.value;
-    while (key !== null){
-      if (key === target){
-        return true;
-      }
-      key = list[key];
-    }
-    return false;
+    // var key = list.tail.value;
+    // while (key !== null){
+    //   if (key === target){
+    //     return true;
+    //   }
+    //   key = list[key];
+    // }
+    // return false;
   };
 
   return list;
