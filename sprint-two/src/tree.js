da277be2164c5ebfa2ef45ad735c799b99c582e3
Hack Reactor Students <sjsnider@gmail.com>
2014-04-17 21:18:46 -0700

    add underscore to specrunner and pass tree tests"

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,8 @@
 var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
   _.extend(newTree, treeMethods);
-  window.alert(_);
   return newTree;
 };
 
@@ -11,8 +10,20 @@ var makeTree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  this.children.push(makeTree(value));
 };
 
 treeMethods.contains = function(target){
+  var result = false;
+  if(this.value === target)
+    result = true;
+  else {
+    for(var i=0; i < this.children.length; i++){
+      if(!result){
+        result = this.children[i].contains(target);
+      }
+    }
+  }
+  return result;
 };
 
