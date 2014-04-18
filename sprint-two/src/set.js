bb552b5bc88634d1df840539586dd7f09afeae70
Hack Reactor Students <sjsnider@gmail.com>
2014-04-18 11:25:47 -0700

    complete set.js

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,10 +7,25 @@ var makeSet = function(){
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  if(this._storage === undefined){
+    this._storage = [];
+
+  }
+  this._storage.push(item);
 };
 
 setPrototype.contains = function(item){
+  var index = this._storage.indexOf(item);
+  if (this._storage[index]===item){
+    return true;
+  }else{
+    return false;
+  }
 };
 
 setPrototype.remove = function(item){
+  var index = this._storage.indexOf(item);
+  if(index!==-1){
+    this._storage.splice(index, 1);
+  }
 };
