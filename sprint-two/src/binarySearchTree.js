27b625ac1534973f22bfd14ac2f5a303a661d79a
Hack Reactor Students <sjsnider@gmail.com>
2014-04-18 16:04:22 -0700

    Add binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,2 +1,62 @@
 var makeBinarySearchTree = function(value){
+  var instance;
+
+  instance = Object.create(binaryMethods);
+  instance.value = value;
+  instance.right = {};
+  instance.left = {};
+
+
+  return instance;
+
+};
+
+var binaryMethods = {};
+
+binaryMethods.insert = function(val){
+
+  if(this.value<val){
+    if(this.right.value === undefined){
+      this.right = makeBinarySearchTree(val);
+    }else{
+      this.right.insert(val);
+    }
+  }else{
+    if(this.left.value === undefined){
+      this.left = makeBinarySearchTree(val);
+    }else{
+      this.left.insert(val);
+    }
+  }
+
+};
+
+
+binaryMethods.contains = function(val){
+  if(this.value===val){
+    return true;
+  }else if(this.value<val){
+    if(this.right.value)
+      return this.right.contains(val);
+  }else{
+    if(this.left.value)
+    return this.left.contains(val);
+  }
+  return false;
+
+};
+
+
+binaryMethods.depthFirstLog = function(func){
+
+
+  func(this.value);
+
+  if(this.left.value){
+    this.left.depthFirstLog(func);
+  }
+  if(this.right.value){
+    this.right.depthFirstLog(func);
+  }
+
 };
