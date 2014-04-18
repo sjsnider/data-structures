85fa34c698a4a3336054f4a777218e791df6c076
Hack Reactor Students <sjsnider@gmail.com>
2014-04-18 12:32:21 -0700

    start hash tables

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,12 +5,29 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  //if (this._storage[i])
+  this._storage.set(i,[k,v]);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  // debugger;
+  var results;
+  this._storage.each(function(value, key, storage){
+    if (value !== undefined){
+      if (value[0] === k)
+        results = value[1];
+    }
+  });
+  return results;
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.each(function(value, key, storage){
+    if(this._storage[i][0] === k){
+      this._storage[i][1] = null;
+    }
+  });
 };
 
